f78949a0519a772b92de13f31ae1bdfbd4a0b06c
Mongesh <mayas1985@gmail.com>
2017-01-06 16:55:37 +0530

    user controller changes

--- NationPost.API/Controllers/RatingsController.cs
+++ NationPost.API/Controllers/RatingsController.cs
@@ -66,7 +66,7 @@ namespace NationPost.API.Controllers
             {
                 db.SaveChanges();
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 throw;
             }
